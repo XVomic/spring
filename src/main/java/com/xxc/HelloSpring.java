0054c00b3b1d4b024dcfc2746375c85c03e9475c
xuxincheng <2961166903@qq.com>
2025-05-21 22:05:18 +0800

    增加基础依赖

---
+++ src/main/java/com/xxc/HelloSpring.java
@@ -0,0 +1,17 @@
+package com.xxc;
+
+public class HelloSpring {
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void show(){
+        System.out.println(userName+"Hello Spring");
+    }
+}
